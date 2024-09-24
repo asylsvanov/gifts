963d54c404c8d1720526031e6e184cbf5f475e58
asylsvanov <asylsvanov@gmail.com>
2024-09-24 10:41:32 +0500

    +7 packages

--- public/index.php
+++ public/index.php
@@ -7,3 +7,21 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+if (!function_exists('dd')) {
+    function dd(...$args)
+    {
+        $origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+        header("Access-Control-Allow-Origin: $origin");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
+        http_response_code(500);
+
+        foreach ($args as $x) {
+            (new Symfony\Component\VarDumper\VarDumper)->dump($x);
+        }
+
+        die(1);
+    }
+}
